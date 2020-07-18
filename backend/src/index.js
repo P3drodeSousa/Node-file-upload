2e37f7f9e4176fe943785897e34923fe8fc240d3
tempname1533 <pedro_de_sousa@outlook.com>
2020-07-18 15:28:20 +0200

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://test:Test2019$@ds331198.mlab.com:31198/tests", {
+  useNewUrlParser: true,
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(require("./routes"));
+
+app.listen(3000);
